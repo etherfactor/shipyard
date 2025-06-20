76e2a365664f0550f8ff7687db1fa8487c373d26
Ryan Duffy <ether.factor@gmail.com>
2025-06-19 20:47:18 -0500

    Refactors error handling and adds missing utilities
    Refactors error reporting to provide more detailed property paths in self-help errors.
    Adds ExpressionExtensions to fetch property info and convert expressions into paths.
    Improves EntityNotFoundReferenceError to include property details.

---
+++ src/EtherGizmos.Shipyard.Models/Api/Errors/Error_Reference.cs
@@ -0,0 +1,49 @@
+﻿using EtherGizmos.Shipyard.Utilities.Extensions;
+using System.Linq.Expressions;
+using System.Net;
+
+namespace EtherGizmos.Shipyard.Models.Api.Errors;
+
+public partial class Error
+{
+    public static class Reference
+    {
+        public class EntityNotFoundReferenceError<TEntity> : TypedErrorBase
+        {
+            private const string _message = "An entity with the provided id was not found.";
+
+            public override HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+            public EntityNotFoundReferenceError(
+                string target = ErrorConstants.RequestTarget.Uri)
+                : base(ErrorConstants.Code.UnsupportedOperation.QueryOptionNotApplicable,
+                      target,
+                      _message)
+            {
+            }
+
+            public EntityNotFoundReferenceError<TEntity> AddDetail(
+                Expression<Func<TEntity, object?>> selector,
+                object? value)
+            {
+                var detail = new SelfErrorDetail(this, selector, value);
+                AddDetail(detail);
+
+                return this;
+            }
+
+            private class SelfErrorDetail : TypedErrorDetailBase
+            {
+                private const string _message = "Non-existent key value: {0}";
+
+                public SelfErrorDetail(
+                    TypedErrorBase parent,
+                    Expression<Func<TEntity, object?>> selector,
+                    object? value)
+                    : base(parent.Code, selector.GetPath(), string.Format(_message, value))
+                {
+                }
+            }
+        }
+    }
+}
