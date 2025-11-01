9f3aa549eb5697834ed2935db2e2a263b6a9a742
Ryan Duffy <ether.factor@gmail.com>
2025-11-01 18:10:10 -0500

    Change OAuth service logic to wait for a new access token when expired

--- src/EtherGizmos.Shipyard.Web/src/app/shared/services/oauth2/oauth2.service.concrete.ts
+++ src/EtherGizmos.Shipyard.Web/src/app/shared/services/oauth2/oauth2.service.concrete.ts
@@ -1,6 +1,8 @@
 import { effect, inject, Injectable, Provider, signal } from "@angular/core";
 import { Router } from "@angular/router";
-import { EventTypes, OidcSecurityService, PublicEventsService } from "angular-auth-oidc-client";
+import { OidcSecurityService } from "angular-auth-oidc-client";
+import { catchError, filter, firstValueFrom, of, switchMap, take, timeout, timer } from "rxjs";
+import { Logger } from "../../utilities/logger/logger.util";
 import { OAuth2Service } from "./oauth2.service";
 
 @Injectable({
@@ -8,11 +10,10 @@ import { OAuth2Service } from "./oauth2.service";
 })
 export class ConcreteOAuth2Service extends OAuth2Service {
 
+  private readonly $logger = inject(Logger).forContext("OAuth2Service");
   private readonly $oidc = inject(OidcSecurityService);
-  private readonly $events = inject(PublicEventsService);
   private readonly $router = inject(Router);
 
-  private authTriggered = false;
   private onReadyResolve!: () => void;
   readonly onReady = new Promise<void>((resolve, reject) => {
     this.onReadyResolve = resolve;
@@ -24,15 +25,8 @@ export class ConcreteOAuth2Service extends OAuth2Service {
 
   constructor() {
     super();
-    this.$oidc.checkAuthIncludingServer().subscribe();
 
-    this.$events.registerForEvents().subscribe(e => {
-      if (e.type === EventTypes.CheckingAuthFinished) {
-        this.authTriggered = true;
-      } else if (e.type === EventTypes.CheckingAuthFinishedWithError) {
-        this.onReadyResolve();
-      }
-    });
+    this.initialize();
 
     effect(() => {
       const auth = this.$oidc.authenticated();
@@ -44,11 +38,50 @@ export class ConcreteOAuth2Service extends OAuth2Service {
       } else {
         this.idTokenData$$.set({});
       }
-
-      if (this.authTriggered) {
-        this.onReadyResolve();
-      }
     });
+  }
+
+  private async initialize() {
+    this.$logger.information("OAuth 2.0 initializing...");
+    const refreshToken = await firstValueFrom(this.$oidc.getRefreshToken());
+
+    if (refreshToken) {
+      this.$logger.information("Found a refresh token");
+      let check = this.$oidc.checkAuthIncludingServer();
+      check = check.pipe(
+        catchError(() => timer(1000).pipe(
+          switchMap(() => check),
+        )),
+      );
+      const response = await firstValueFrom(check);
+      if (response.isAuthenticated) {
+        //If the session is authenticated, it will function as expected
+        this.$logger.information("Session is currently authenticated");
+        this.onReadyResolve();
+      } else {
+        this.$logger.information("Session is not currently authenticated; will wait for the next access token");
+        const nowAccessToken = response.accessToken;
+
+        //The session expired, so we want to wait for the new access token to come back
+        timer(0, 200).pipe(
+          switchMap(() => this.$oidc.getAccessToken()),
+          filter(token => token !== nowAccessToken),
+          timeout(60000),
+          catchError(() => of("")),
+          take(1),
+        ).subscribe(() => {
+          //We now have the access token and are ready to go
+          this.$logger.information("Found a new access token");
+          this.onReadyResolve();
+        });
+      }
+    } else {
+      this.$logger.information("No refresh token found");
+      await firstValueFrom(this.$oidc.checkAuth());
+
+      //There's no session, so we can start whenever
+      this.onReadyResolve();
+    }
   }
 
   login(): void {
