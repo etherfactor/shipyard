261cee1206c1be4cc635b1614bb6172a4bb0c6f6
Ryan Duffy <ether.factor@gmail.com>
2026-09-01 21:41:34 -0500

    Remove log statement

--- src/EtherGizmos.Shipyard.Web/src/app/features/notification/pages/notification-subscription-detail/notification-subscription-detail.component.ts
+++ src/EtherGizmos.Shipyard.Web/src/app/features/notification/pages/notification-subscription-detail/notification-subscription-detail.component.ts
@@ -325,7 +325,6 @@ export class NotificationSubscriptionDetailComponent implements OnInit {
     if (form.invalid) {
       form.markAsUntouched();
       form.markAllAsTouched();
-      console.log(form);
       return;
     }
 
